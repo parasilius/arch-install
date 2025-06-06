7287e57a83cb6f9186d3c3584613b90eac0f9010
Parsa H. Rafsanjani <parsahosseini2001@gmail.com>
2025-06-06 16:40:36 +0330

    Add timezone as a variable to installer

--- install.sh
+++ install.sh
@@ -78,7 +78,7 @@ install_packages() {
 configure_system() {
     genfstab -U /mnt >> /mnt/etc/fstab
     arch-chroot /mnt
-    ln -sf /usr/share/zoneinfo/Iran /etc/localtime
+    ln -sf /usr/share/zoneinfo/"$TIMEZONE" /etc/localtime
     hwclock --systohc
     echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
     locale-gen
