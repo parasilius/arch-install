60d460c91b967b1713a61a992ce977ad4660b232
Parsa H. Rafsanjani <parsahosseini2001@gmail.com>
2025-06-06 09:55:25 +0330

    Add network config

--- install.sh
+++ install.sh
@@ -7,3 +7,17 @@ TIMEZONE='Iran'
 WIRELESS='wlan0'
 WIFI_PASSWORD='wifipassword'
 WIFI_SSID='wifi'
+
+network_config() {
+    iwctl
+        station "$WIRELESS" scan
+        station "$WIRELESS" connect "$WIFI_SSID"
+        if [ -z "$WIFI_PASSWORD" ]
+        then
+            echo 'Enter the WiFi password:'
+            stty -echo
+            read WIFI_PASSWORD
+            stty echo
+        fi
+        echo "$WIFI_PASSWORD"
+}
